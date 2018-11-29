a1d7e63eac86bdbc06f0e0dd2f50b8493aa3917f
Tarun7827 <tarun7827@gmail.com>
2018-11-30 00:32:05 +0530

    Final

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,7 @@ public class PlayerControl : MonoBehaviour
 
 	float initialY;
 	float finalY;
+float finalZ;
 
 
 	void Start ()
